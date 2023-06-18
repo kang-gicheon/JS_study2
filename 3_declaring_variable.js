8a2e40f56149104405cd5f59073e4014e3ca6c62
kanggicheon <snoopy1516@naver.com>
2023-06-18 23:13:42 +0900

    variable
    변수선언 학습하기

---
+++ 3_declaring_variable.js
@@ -0,0 +1,43 @@
+/**
+ * 
+ * 1) var
+ * 2) let
+ * 3) const
+ */
+
+var name = "다시시작";
+console.log(name);
+
+var age = 32;
+console.log(age);
+
+let ive = '아이브';
+console.log(ive);
+
+/**
+ * let과 var로 선언하면
+ * 값 추후 변경 가능
+ */
+
+ive = '안유진';
+console.log(ive);
+
+const newJeans = '뉴진스';
+console.log(newJeans);
+
+// newJeans = '뉴진스 멤버 몰라'
+
+/**
+ * 선언과 할당
+ * 
+ * 1) 변수를 선언하는 것
+ * 2) 할당
+ */
+var name; // 변수 선언
+var name = 'ㅎㅇ'; // 할당
+console.log(name);
+
+let girlfriend;
+console.log(girlfriend);
+
+// const girlfriend2; 값을 초기화 하지 않고 작성했을시 오류발생
