55dee5b325fe476c4d7765c9df2e5da7695a1e69
kanggicheon <snoopy1516@naver.com>
2023-06-18 23:16:05 +0900

    Hoisting
    호이스팅 실습

---
+++ 6_hoisting.js
@@ -0,0 +1,24 @@
+/**
+ * Hoisting
+ */
+console.log('Hello');
+console.log('world');
+console.log('-----------------------')
+
+// console.log(name);
+// var name = '팩토리';
+// console.log(name);
+
+/**
+ * Hoisting이란?
+ * 
+ * 모든 변수 선언문이 코드의 최상단으로 이동되는 것처럼 느껴지는 현상을 이야기함
+ */
+// var name;
+// console.log(name);
+// name = '팩토리';
+// console.log(name);
+
+console.log(yuJin);
+let yuJin = '안유진';
+
