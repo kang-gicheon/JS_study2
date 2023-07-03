51999f32729bc6da61117d4ef5266a8f1aa0be82
kanggicheon <snoopy1516@naver.com>
2023-07-03 22:59:59 +0900

    Object +
    1. 객체의 특징
    2. 키 값과 value의 값 가져오기
    추가 학습

--- 14_object.js
+++ 14_object.js
@@ -50,3 +50,42 @@ console.log(color2);
 
 delete color2['newColor'];
 console.log(color2);
+
+/**
+ * 객체의 특징
+ * 
+ * 1) const로 선언할 경우 객체 자체 변경 불가능
+ * 2) 객체 안의 프로퍼티 / 메서드는 변경 가능
+ */
+
+const black = {
+
+    name: '검은색',
+    title: '무채색'
+
+};
+console.log(black);
+
+// black = {}; 특징 1번의 경우에 해당 Assignment to constant variable. 에러 발생
+
+black['name'] = '회색';
+console.log(black); // 특징 2번의 경우에 해당한다
+
+/**
+ * 키 값을 가져오기
+ */
+console.log(Object.keys(black));
+
+/**
+ * 모든 value 값 가져오기
+ */
+console.log(Object.values(black));
+
+const name = '보라';
+
+const color3 = {
+
+    name // = name = name과 같음 
+
+};
+console.log(color3);
